f02cf8d61652adde1c0069a942148535edb933e8
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2024-03-11 04:07:43 +0000

    changed the layout of the unset goal for addition, first step for initiating the transition to detail task screens through a button

--- components/HomePlusButton.tsx
+++ components/HomePlusButton.tsx
@@ -1,36 +1,37 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import { View, Text } from "./Themed";
 import { router } from "expo-router";
 
 const HomePlusButton = (props: { widht: number; height: number }) => {
+  const { widht, height } = props;
+
   return (
     <TouchableOpacity
-    onPress={() => {
-      router.push("/(notification)"); // change path to mood check-in
-    }}
-    >
-    <View
-      style={{
-        height: props.height,
-        width: props.widht,
-        backgroundColor: "#DFDAEC",
-        borderRadius: 12,
-        justifyContent: "center",
-        alignItems: "center",
+      onPress={() => {
+        router.push("/(notification)"); // change path to mood check-in
       }}
     >
-      <Text
-        style={{
-          textAlign: "center",
-          fontSize: 25,
-          fontFamily: "Rubik_400Regular",
-        }}
-      >
-        +
-      </Text>
-    </View>
+      <View style={[styles.container, { height, width: widht }]}>
+        <Text style={[styles.text, { lineHeight: height }]}>
+          +
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#DFDAEC",
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 30,
+    fontFamily: "Rubik_300Light",
+  },
+});
 
 export default HomePlusButton;
