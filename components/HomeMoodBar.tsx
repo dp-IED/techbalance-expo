3a031b59301f3219cbb0bb5de409b1fc987f1818
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2024-03-11 04:02:01 +0000

    clear code

--- components/HomeMoodBar.tsx
+++ components/HomeMoodBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Image } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import { Slider } from "react-native-elements";
 import HomePlusButton from "./HomePlusButton";
 
@@ -7,15 +7,7 @@ const HomeMoodBar = () => {
   const [value, setValue] = useState(0);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        //  alignItems: "stretch",
-        justifyContent: "center",
-        // padding: 20,
-        paddingVertical: 20,
-      }}
-    >
+    <View style={styles.container}>
       <Slider
         value={value}
         onValueChange={setValue}
@@ -23,47 +15,54 @@ const HomeMoodBar = () => {
         minimumValue={0}
         step={1}
         disabled={true}
-        // thumbStyle={{ height: 0 }}
-        trackStyle={{
-          height: 40,
-          borderRadius: 20,
-        }}
-        thumbStyle={{
-          height: 0,
-          width: 0,
-          borderRadius: 0,
-        }}
+        trackStyle={styles.track}
+        thumbStyle={styles.thumb}
         thumbTintColor="transparent"
         minimumTrackTintColor="#DFDAEC"
         maximumTrackTintColor="#F6F5FA"
       />
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          position: "absolute",
-          width: "100%",
-          paddingHorizontal: 8,
-        }}
-      >
+      <View style={styles.row}>
         <Image
           source={require("../assets/images/home_pb_smile_icon.png")}
-          style={{
-            height: 32,
-            width: 32,
-          }}
+          style={styles.icon}
         />
         <HomePlusButton widht={36} height={36} />
         <Image
           source={require("../assets/images/home_pb_sad_icon.png")}
-          style={{
-            height: 32,
-            width: 32,
-          }}
+          style={styles.icon}
         />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingVertical: 20,
+  },
+  track: {
+    height: 40,
+    borderRadius: 20,
+  },
+  thumb: {
+    height: 0,
+    width: 0,
+    borderRadius: 0,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    width: "100%",
+    paddingHorizontal: 8,
+    alignItems: "center",
+  },
+  icon: {
+    height: 32,
+    width: 32,
+  },
+});
 
 export default HomeMoodBar;
